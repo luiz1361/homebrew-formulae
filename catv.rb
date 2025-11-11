fba4e4f2eb085e489f436a74c99b114c811d0dc3
Luiz F. C. Martins <luiz1361@gmail.com>
2025-11-11 13:48:47 +0000

    Update Catv formula to support Intel architecture with correct URL and placeholder SHA256

--- catv.rb
+++ catv.rb
@@ -5,8 +5,13 @@ class Catv < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/luiz1361/catv/releases/download/v0.0.5/catv-darwin-arm64"
-    sha256 "467e6efd34c5d69e49f57068655947ac140dd0bce0fd52a2f358290c3ce35239"
+    if Hardware::CPU.arm?
+      url "https://github.com/luiz1361/catv/releases/download/v0.0.5/catv-darwin-arm64"
+      sha256 "467e6efd34c5d69e49f57068655947ac140dd0bce0fd52a2f358290c3ce35239"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/luiz1361/catv/releases/download/v0.0.5/catv-darwin-amd64"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_AMD_BINARY"
+    end
   end
 
   def install
