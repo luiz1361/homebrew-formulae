1c2b22341462745467c87ba5e430a3e6f4858644
Luiz F. C. Martins <239121271+luiz1361@users.noreply.github.com>
2026-01-14 23:27:36 +0000

    Moving repository to jae-labs org

--- catv.rb
+++ catv.rb
@@ -1,15 +1,15 @@
 class Catv < Formula
   desc "Cards Against the Void - AI-powered flashcards CLI"
-  homepage "https://github.com/luiz1361/catv"
+  homepage "https://github.com/jae-labs/catv"
   version "v0.0.18"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/luiz1361/catv/releases/download/v0.0.5/catv-darwin-arm64"
+      url "https://github.com/jae-labs/catv/releases/download/v0.0.5/catv-darwin-arm64"
       sha256 "467e6efd34c5d69e49f57068655947ac140dd0bce0fd52a2f358290c3ce35239"
     elsif Hardware::CPU.intel?
-      url "https://github.com/luiz1361/catv/releases/download/v0.0.5/catv-darwin-amd64"
+      url "https://github.com/jae-labs/catv/releases/download/v0.0.5/catv-darwin-amd64"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_AMD_BINARY"
     end
   end
@@ -18,7 +18,7 @@ class Catv < Formula
     # Rename the binary during install
     bin.install Dir["catv-*"].first => "catv"
   end
-  
+
   test do
     system "#{bin}/catv", "--version"
   end
